fe8b66a62c00e8a53599facb099f2b367a417c9c
leesovska <leesovska@gmail.com>
2024-02-25 11:45:09 +0300

    Домашнее задание 4

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const inputLine = document.querySelector('#input');
+const items = document.querySelector('.items');
+
+inputLine.addEventListener('keydown', function(event) {
+  if (event.key == 'Enter') {
+    const text = inputLine.value;
+
+    if (text) {
+      const newItem = document.createElement('li');
+      newItem.textContent = text;
+      newItem.addEventListener('click', function() {
+        newItem.classList.toggle('done');
+      })
+      items.append(newItem);
+      inputLine.value = '';
+    }
+  }
+});
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
